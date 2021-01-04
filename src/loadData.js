a62fdf0ab507d310bf2d402eb09f43f3654db381
Andre Masson <andre.masson@survata.com>
2021-01-04 17:48:11 -0600

    Added user entities

--- src/loadData.js
+++ src/loadData.js
@@ -3,6 +3,33 @@ const fs = require('fs');
 AWS.config.update({ region: 'local', endpoint: 'http://localhost:8000' });
 var docClient = new AWS.DynamoDB.DocumentClient();
 
+// Users sample
+// -----------------------------------
+console.log('Importing users:');
+const users = JSON.parse(fs.readFileSync('src/data/users.json', 'utf8'));
+
+users.forEach(function (user) {
+    console.log(`\ - ${user.email}`);
+
+    const uuid = user.uuid;
+    const params = {
+        TableName: 'authorization_local',
+        Item: {
+            pkey: `user:${uuid}`,
+            skey: `metadata:${uuid}`,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email
+        }
+    };
+
+    docClient.put(params, function (err, _data) {
+        if (err) {
+            console.error(err);
+        }
+    });
+});
+
 // Accounts sample
 // -----------------------------------
 console.log('Importing accounts:');
