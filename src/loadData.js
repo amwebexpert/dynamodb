4005b6d640ee16d75c57e8f5a01bfc66e32a2a5f
Andre Masson <andre.masson@survata.com>
2021-01-04 17:29:47 -0600

    Added team records

--- src/loadData.js
+++ src/loadData.js
@@ -6,26 +6,52 @@ var docClient = new AWS.DynamoDB.DocumentClient();
 // Accounts sample
 // -----------------------------------
 console.log('Importing accounts:');
-const accounts = JSON.parse(fs.readFileSync('src/accounts.json', 'utf8'));
+const accounts = JSON.parse(fs.readFileSync('src/data/accounts.json', 'utf8'));
 
 accounts.forEach(function (account) {
-    console.log(account);
+    console.log(`\ - ${account.name}`);
 
+    const uuid = account.uuid;
     const params = {
         TableName: 'authorization_local',
         Item: {
-            pkey: `account:${account.pkey}`,
-            skey: `metadata:${account.pkey}`,
+            pkey: `account:${uuid}`,
+            skey: `metadata:${uuid}`,
             name: account.name,
             campaigns: account.campaigns
         }
     };
 
-    docClient.put(params, function (err, data) {
+    docClient.put(params, function (err, _data) {
         if (err) {
             console.error(err);
-        } else {
-            console.log(`PutItem succeeded for ${account.name}`, data);
+        }
+    });
+});
+
+// Teams sample
+// -----------------------------------
+console.log('Importing teams:');
+const teams = JSON.parse(fs.readFileSync('src/data/teams.json', 'utf8'));
+
+teams.forEach(function (team) {
+    console.log(`\ - ${team.name}`);
+
+    const accountUuid = team.accountUuid;
+    const uuid = team.uuid;
+    const params = {
+        TableName: 'authorization_local',
+        Item: {
+            pkey: `account:${accountUuid}`,
+            skey: `team:${uuid}`,
+            name: team.name,
+            subjects: team.subjects
+        }
+    };
+
+    docClient.put(params, function (err, _data) {
+        if (err) {
+            console.error(err);
         }
     });
 });
